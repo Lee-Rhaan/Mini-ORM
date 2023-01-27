4120d0d1e5c732699222cbce32537d9368fc0c4b
Lee-Rhaan <rhaanking@gmail.com>
2023-01-27 11:31:52 +0200

    Initial Commit

---
+++ src/main/java/com/mini/orm/miniorm/MiniOrmApplication.java
@@ -0,0 +1,13 @@
+package com.mini.orm.miniorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class MiniOrmApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(MiniOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mini/orm/miniorm/MiniOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini.orm.miniorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
